988b314d443c01e1712f2b9d71a6c0f24e21db04
Chandan <113888446+chandandevz@users.noreply.github.com>
2024-05-23 19:45:49 +0530

    Update sourcecode.js

--- sourcecode.js
+++ sourcecode.js
@@ -1,1 +1,7 @@
+const toggleButton = document.querySelector('.open-menu');
+const nav = document.querySelector('.nav-bar');
+
+toggleButton.addEventListener('click',() => {
+    nav.classList.toggle('active')
+})
 
